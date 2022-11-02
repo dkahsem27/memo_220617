cb6a78f312a8594f38d258a262b488cb43513af5
dkahsem27 <dkahsem27@gmail.com>
2022-11-02 20:04:32 +0900

    1102 인터셉터(Interceptor)

--- src/main/java/com/memo/config/WebMvcConfig.java
+++ src/main/java/com/memo/config/WebMvcConfig.java
@@ -1,18 +1,31 @@
 package com.memo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.memo.common.FileManagerService;
+import com.memo.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+	@Autowired
+	private PermissionInterceptor interceptor;
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry
 		.addResourceHandler("/images/**") // 웹 주소(url path) http://localhost/images/dkahsem27_1620204641/sun.png
 		.addResourceLocations("file:///" + FileManagerService.FILE_UPLOAD_PATH); // 실제 파일 위치 경로(이미지)
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor)
+		.addPathPatterns("/**") //  /post/post_list_view      /** 아래 디렉토리까지 확인
+		.excludePathPatterns("/favicon.ico", "/error", "/user/sign_out", "/static/**");
 	}
 }
